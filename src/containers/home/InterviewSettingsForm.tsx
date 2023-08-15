8e61950d44d6976cc995766bf6280b60715af776
Debangshu <debangshu182002@gmail.com>
2023-08-15 17:14:35 +0530

    done

---
+++ src/containers/home/InterviewSettingsForm.tsx
@@ -0,0 +1,108 @@
+import { Button, Flex, Box } from "@chakra-ui/react";
+import React from "react";
+import FormSelect from "../../components/formComponents/FormSelect";
+import { useFormik } from "formik";
+import { PageNumbers } from "../../interface/home";
+import { IInterViewSettings } from "../../interface/forms";
+import {
+  interviewDurationOptions,
+  interviewLanguageOptions,
+  interviewModeOptions,
+} from "./constants";
+import { useData } from "./DataProvider";
+import * as Yup from "yup";
+
+
+const InterviewDetailsForm: React.FC<{
+  handleTab: (n: PageNumbers) => void;
+}> = ({ handleTab }) => {
+
+  const { state, setState } = useData();
+
+  const handleChange = (e) => {
+    const { name, value } = e.target;
+    setState((prevState) => ({
+      ...prevState,
+      interviewSettings: {
+        ...prevState.interviewSettings,
+        [name]: value,
+      },
+    }));
+  };
+
+  const {
+    errors,
+    touched,
+    handleSubmit,
+    values,
+    setFieldTouched,
+    setFieldValue,
+  } = useFormik<IInterViewSettings>({
+    initialValues: state.interviewSettings,
+    enableReinitialize: true,
+    validationSchema: Yup.object().shape({
+      
+      interviewMode: Yup.string().required("interviewMode is required"),
+      interviewDuration: Yup.string().required("interviewDuration is required"),
+      interviewLanguage: Yup.string().required("interviewLanguage is required"),
+    }),
+    
+    onSubmit: (values) => {
+      console.log({ values });
+      alert("Form successfully submitted");
+    },
+  });
+
+  return (
+    <Box width="100%" as="form" onSubmit={handleSubmit as any}>
+      <Box width="100%">
+        <FormSelect
+          label="Interview Mode"
+          placeholder="Select interview mode"
+          name="interviewMode"
+          options={interviewModeOptions}
+          onChange={setFieldValue}
+          onBlur={setFieldTouched}
+          value={values?.interviewMode}
+          error={errors?.interviewMode}
+          touched={touched?.interviewMode}
+          setState={setState}
+        />
+        <FormSelect
+          label="Interview Duration"
+          placeholder="Select interview duration"
+          name="interviewDuration"
+          options={interviewDurationOptions}
+          onChange={setFieldValue}
+          onBlur={setFieldTouched}
+          value={values?.interviewDuration}
+          error={errors?.interviewDuration}
+          touched={touched?.interviewDuration}
+          setState={setState}
+        />
+        <FormSelect
+          label="Job Location"
+          name="interviewLanguage"
+          placeholder="Select interview language"
+          options={interviewLanguageOptions}
+          onChange={setFieldValue}
+          onBlur={setFieldTouched}
+          error={errors.interviewLanguage}
+          touched={touched.interviewLanguage}
+          value={values.interviewLanguage}
+          setState={setState}
+        />
+        <Flex w="100%" justify="flex-end" mt="4rem" gap="20px">
+          <Button colorScheme="gray" type="button" onClick={() => handleTab(1)}>
+            Previous
+          </Button>
+          <Button colorScheme="red" type="submit">
+            Submit
+          </Button>
+        </Flex>
+      </Box>
+    </Box>
+  );
+};
+
+export default InterviewDetailsForm;
